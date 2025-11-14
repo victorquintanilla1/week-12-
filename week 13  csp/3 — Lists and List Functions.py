323a6f0154512f9598b82fa3b59e9445ba2ed3fc
victorquintanilla1 <vjquintanill@cps.edu>
2025-11-14 20:37:09 +0000

    lists

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -6,6 +6,36 @@
 
 # Examples:
 
+# collections are used to store multiple items in a single variable
+# lists are ordered collcections of items
+# lists are mutable, meaning you can change their content
+# lists are created using square brackets []
+# instead if creating seperate variables
+# for each item, we cab store them in a list
+# this makes our job easier
+# when we need to manage multiple items
+# performance task answer
+
+my_list = [1,2,3,4,5]
+print(my_list)
+print(type(my_list))
+print(my_list[1:4])
+print(my_list[0:])
+# moldifying list
+# adding an item to the end of the list
+my_list.append(6)
+my_list.append(7)
+my_list.append(8)
+print(my_list)
+my_list.append((9,10,11,12,13,14))
+print(my_list)
+# add 500 more numbers to the list
+my_list.extend(list(range(15, 515)))
+print(my_list)
+my_list.extend(list(range(515, 1115)))
+print(my_list)
+
+
 my_list = ['apple', 'banana', 'cherry']
 print(my_list[0])         # apple
 print(my_list[1:])        # ['banana', 'cherry']
@@ -19,6 +49,8 @@ print(my_list)
 numbers = [3, 1, 4, 2]
 numbers.sort()
 print(numbers)
+
+
 
 
 # Practice Problems:
